ca6981a69e710ad5c78fa5e00c6533723b249361
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2021-09-14 21:10:02 -0700

    Add safety check for pgcrypto

--- db/migrate/20200703075900_enable_uuid.rb
+++ db/migrate/20200703075900_enable_uuid.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class EnableUuid < ActiveRecord::Migration[6.0]
   def change
+    return if extension_enabled? 'pgcrypto'
     enable_extension 'pgcrypto'
   end
 end
