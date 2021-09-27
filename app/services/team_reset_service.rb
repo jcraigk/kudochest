8dd344e82ad3c4a6ec29eb9b1b99b31f4504ccdb
Justin Craig-Kuhn <jcraigk@gmail.com>
2021-09-27 17:46:35 -0600

    Do not DM users on stats reset (#80)

--- app/services/team_reset_service.rb
+++ app/services/team_reset_service.rb
@@ -15,7 +15,6 @@ class TeamResetService < Base::Service
     team.profiles.find_each do |profile|
       destroy_tips(profile)
       reset_profile_stats(profile)
-      notify_user(profile, t('teams.stats_reset'))
     end
     team.update!(points_sent: 0)
   end
@@ -36,16 +35,5 @@ class TeamResetService < Base::Service
     Tip.where(to_profile: profile)
        .or(Tip.where(from_profile: profile))
        .delete_all
-  end
-
-  def notify_user(profile, text)
-    return unless profile.allow_dm?
-    "#{team.plat}::PostService".constantize.call(
-      team_rid: team.rid,
-      profile_rid: profile.rid,
-      mode: :direct,
-      text: text,
-      team_config: team.config
-    )
   end
 end
