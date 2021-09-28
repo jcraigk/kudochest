c73b7cfcbd53703088997e38f6e35567e5390396
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2021-09-28 12:45:48 -0700

    Configure SidekiqUniqueJobs::Middleware

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,6 +10,24 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = redis_config
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
 
 # https://github.com/mperham/sidekiq/wiki/Monitoring
