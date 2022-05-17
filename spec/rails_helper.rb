2d4f78f531005fe7e3677821b84fd8c0b439cc8d
Justin Craig-Kuhn <jcraigk@gmail.com>
2022-05-16 23:18:50 -0700

    Remove database_cleaner, use_transactional_fixtures=true (#175)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.order = 'random'
