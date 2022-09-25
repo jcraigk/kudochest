efede41224027d45a0989537845e72ced0ffa852
Justin Craig-Kuhn <jcraigk@gmail.com>
2022-09-24 22:15:54 -0700

    Bundle update, yarn update (#181)

--- babel.config.js
+++ babel.config.js
@@ -54,23 +54,27 @@ module.exports = function(api) {
         }
       ],
       [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false,
-          regenerator: true,
-          corejs: false
+          helpers: false
         }
       ],
       [
         '@babel/plugin-transform-regenerator',
         {
           async: false
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true 
         }
       ]
     ].filter(Boolean)
